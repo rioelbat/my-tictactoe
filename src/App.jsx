973d98b7d60844b243d67b5638c96e71c7e6377c
unknown <rio.elwi@gmail.com>
2023-02-16 12:49:05 +0700

    Add new 9 squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
-import './App.css';
-
-export default function App () {
-  return <button type="button" className="square">O</button>
+export default function Square() {
+  return (
+    <>
+      <div className="board-row">
+        <button type="button" className="square">
+          1
+        </button>
+        <button type="button" className="square">
+          2
+        </button>
+        <button type="button" className="square">
+          3
+        </button>
+      </div>
+      <div className="board-row">
+        <button type="button" className="square">
+          4
+        </button>
+        <button type="button" className="square">
+          5
+        </button>
+        <button type="button" className="square">
+          6
+        </button>
+      </div>
+      <div className="board-row">
+        <button type="button" className="square">
+          7
+        </button>
+        <button type="button" className="square">
+          8
+        </button>
+        <button type="button" className="square">
+          9
+        </button>
+      </div>
+    </>
+  );
 }
