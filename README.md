30c4fcce2eae09417ec419d986bcba7d62404395
rioelbat <rio.elwi@gmail.com>
2023-02-25 16:13:05 +0700

    Add Move History features and update styling

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-# Simple Tictactoe
+# TicTacToe
 
-This is a simple TicTacToe react based application. I built this while i am learning how to use React library.
-I am using `React v18.2.0` to build this `TicTacToe`
+<p align="center">
+  <img src="public/preview.PNG" alt="My Tictactoe Preview">
+</p>
+
+## Description
+
+This is a simple TicTacToe react based application. I built this while i was learning React library from [beta.reactjs.org](https://beta.reactjs.org/learn/).tutorial-tic-tac-toe#adding-time-travel.
+
+## Features
+
+- Tictactoe board
+- Status game
+- Move history
+- Reset game
+
+## Stacks
+
+- `React v18.2.0`
+- `Vite v4.1.0`
+
+## How to run it
+
+1. Clone it with this command on your terminal.
+
+```
+git clone https://github.com/rioelbat/my-tictactoe.git
+```
+
+2. Open `my-tictactoe`
+
+3. Install the dependecies
+
+```
+npm install
+```
+
+4. Run the application in dev environtment
+
+```
+npm run dev
+```
--- src/App.jsx
+++ src/App.jsx
@@ -28,37 +28,41 @@ function calculateWinner(squares) {
   return null;
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Status({squares, xIsNext, move}) {
+  const result = calculateWinner(squares);
+  let statusText;
+  if (result === null && move === 9) {
+    statusText = 'draw 🤝🏽';
+  } else if (result === null) {
+    statusText = `${xIsNext ? 'X' : 'O'} play`;
+  } else {
+    statusText = `${result} win 🎉`;
+  }
 
-  function handleClick(value) {
-    if (calculateWinner(squares) || squares[value]) {
+  return (
+    <div className='status'>Status: <b>{statusText}</b></div>
+  );
+}
+
+function Board({xIsNext, squares, move, onPlay}) {
+  function handleClick(squareIndex) {
+    if (calculateWinner(squares) || squares[squareIndex]) {
       return;
     }
 
     const nextSquares = squares.slice();
 
     if (xIsNext) {
-      nextSquares[value] = 'X';
+      nextSquares[squareIndex] = 'X';
     } else {
-      nextSquares[value] = 'O';
+      nextSquares[squareIndex] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = `Status: ${winner} win`;
-  } else {
-    status = `Status: ${xIsNext ? 'X' : 'O'} play`;
+    onPlay(nextSquares);
   }
 
   return (
     <>
-      <div className="status">{status}</div>
+      <Status  xIsNext={xIsNext} squares={squares} move={move} />
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -75,5 +79,59 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  const handlePlay = (nextSquares) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  function reset() {
+    setCurrentMove(0);
+    setHistory([Array(9).fill(null)]);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description = 'Game Start';
+    if (move > 0) {
+      description = `Move #${move}`;
+    }
+
+    return (
+      // eslint-disable-next-line react/no-array-index-key
+      <li key={move}>
+        Go to <button className="btn" type="button" onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );    
+  });
+
+  return (
+    <div className="container">
+      <button className="btn" type="button" onClick={() => reset()}>Reset</button>
+      <div className="title">
+        Tic-Tac-Toe
+      </div>
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} move={currentMove} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          Move History:
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    </div>
   );
 }
